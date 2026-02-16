b1e7923397013e9c16cb48bd2d023362c3a743f7
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-02-16 02:10:13 -0500

    SCRUM-19 refactor: update links for Express compatibility

--- frontend/static/js/auth.js
+++ frontend/static/js/auth.js
@@ -47,7 +47,7 @@ if (loginForm) {
     e.preventDefault();
 
     try {
-    const response = await fetch("http://localhost:3000/api/auth/login", {
+    const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
         "Content-Type": "application/json"
@@ -91,7 +91,7 @@ if (registerForm) {
     }
 
     try {
-    const response = await fetch("http://localhost:3000/api/auth/register", {
+    const response = await fetch("/api/auth/register", {
         method: "POST",
         headers: {
         "Content-Type": "application/json"
