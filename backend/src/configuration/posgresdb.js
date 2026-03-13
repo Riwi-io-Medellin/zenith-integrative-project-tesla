34859062b2a62c391504feda7dccb3959ebc312d
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-03-13 02:12:37 -0500

    SCRUM-32 refactor: server configuration

--- backend/src/configuration/posgresdb.js
+++ backend/src/configuration/posgresdb.js
@@ -24,9 +24,10 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PWD,
   port: process.env.DB_PORT,
-  options: `-c search_path=${process.env.DB_SCHEMA || 'public'},public`
+  options: `-c search_path=${process.env.DB_SCHEMA || 'public'},public`,
+  connectionTimeoutMillis: 15000
 });
 
 export const connectPg = async () => {
